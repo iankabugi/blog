cdc3cf469363d2df80aaf01d3dbd81512c917b7e
iankabugi <iank299@gmail.com>
2019-02-19 14:48:35 +0300

    configure  manage.py to function as expected

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
-import unittest
-from flask_script import Manager,Server
-from app import create_app,db
-from  flask_migrate import Migrate, MigrateCommand
-from app.models import User,Role
 
-app = create_app('default')
+from flask_script import Manager,Server
+from app import create_app, db
+from  flask_migrate import Migrate, MigrateCommand
+from app.models import User, Role
+
+app = create_app('development')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
@@ -14,13 +14,14 @@ manager.add_command('db',MigrateCommand)
 @manager.command
 def test():
     """Run the unit tests."""
+    import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User Role = Role )
+    return dict(app = app,db = db,User = User, Role = Role )
 
 
 if __name__ == '__main__':
