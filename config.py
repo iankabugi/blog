8d0e79be6f27c98e3b832a7ea8214ef4a4bbc43b
iankabugi <iank299@gmail.com>
2019-02-19 14:47:49 +0300

    add emaila configuration

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 import os
 
 class Config:
-
+    email configurations
+    MAIL_SERVER = 'iank299@gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ian:sawasawa@localhost/blog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     @staticmethod
     def init_app(app):
@@ -18,7 +25,7 @@ class DevConfig(Config):
     DEBUG = True
 
 config_options ={"production":ProdConfig,
-"default":DevConfig,"testing":TestConfig
+"development":DevConfig,"testing":TestConfig
 
 }
 
